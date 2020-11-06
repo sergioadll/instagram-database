edb74c68b56cc83547f33749ebffb9e41c40e607
sergioadll <sergio.adll@hotmail.com>
2020-11-06 00:16:49 +0000

    done

--- src/models.py
+++ src/models.py
@@ -1,12 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 db = SQLAlchemy()
+
+class MediaType(enum.Enum):
+    VIDEO = 1
+    PHOTO = 2
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    last = db.Column(db.String(80), unique=False, nullable=False)
+
+    posts = db.relationship('Post',backref='user', lazy=True)
+    comments = db.relationship('Comment',backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +25,59 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "last": self.last,
+        }
+
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))    
+
+    medias = db.relationship('Media',backref='post', lazy=True)
+    comments = db.relationship('Comment',backref='post', lazy=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+        }
+
+class Media(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    media_type = db.Column(db.Enum(MediaType), default=MediaType.PHOTO, nullable=False)
+    url = db.Column(db.String(120), unique=False, nullable=False)
+
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"))    
+
+
+
+    def __repr__(self):
+        return '<User %r>' % self.url
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            ##"media_type": self.media_type,
+            "url": self.url,
+        }
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(400), unique=False, nullable=False)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))    
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"))    
+
+    def __repr__(self):
+        return '<User %r>' % self.comment
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "comment": self.comment,
         }
